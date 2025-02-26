3016efcb524bb65ad13561111db7c948f5e1b2fd
SadidSD <sadidcoc123@gmail.com>
2025-02-26 22:01:26 +0600

    Responsiveness Update

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import Sold from './Images/sold.png'
 
 function App() {
   return (
-    <div className="App flex flex-col justify-center items-center h-screen gap-7 bg-[#efe5db]"> 
-    <img className='h-[300px] w-auto' src={Logo} alt="" />
-      <div className='flex gap-7'>
+    <div className="App flex flex-col justify-center items-center sm:h-screen gap-7 bg-[#efe5db] p-6"> 
+    <img className="h-[200px]  lg:h-[300px] w-auto" src={Logo} alt="Logo" />
+    <div className='flex flex-col gap-7 sm:flex-row'> 
         <Card name="Inventory" image={Inventory}/>
         <Card name="sold Items" image={Sold}/>
       </div>
-      <div className='flex  gap-7'>
+    <div className='flex flex-col gap-7 sm:flex-row'> 
         <Card name="Tasks" image={Tasks}/>
         <Card name="Analytics" image={Analytics}/>
-      </div>
+    </div>
 
 
  
--- src/Components/card.js
+++ src/Components/card.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Card({ name, image }) {
   return (
-    <div className="rounded-3xl shadow-[10px_10px_15px_rgba(0,0,0,0.2)] w-[300px]
+    <div className="rounded-3xl shadow-[10px_10px_15px_rgba(0,0,0,0.2)] sm:w-[300px] w-[230px] 
      items-center justify-center flex flex-col p-2 bg-[#c5b4a2] 
      hover:shadow-[10px_10px_15px_rgba(0,0,0,0.4)] transition duration-300 ease-in-out hover:scale-110
      hover:cursor-pointer">
-      <img src={image} alt="icon" className="h-[200px] w-auto " />
+      <img src={image} alt="icon" className="sm:h-[200px] h-[130px] w-auto " />
       <div className="">
-        <h1 className="text-3xl font-bold font-mono">{name}</h1>
+        <h1 className="sm:text-3xl text-lg font-bold font-mono">{name}</h1>
       </div>
     </div>
   )
